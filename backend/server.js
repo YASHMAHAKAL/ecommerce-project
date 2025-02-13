5ea0f865618fdee10905769c724800d9136c0a4e
YASHMAHAKAL <137387124+YASHMAHAKAL@users.noreply.github.com>
2025-02-13 20:39:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 // Define PORT (Default: 5000)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Start the server
 app.listen(PORT, () => {
